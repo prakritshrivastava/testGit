fec4d0ebe1136a8917e08b64d05d7439318cab3f
Prakrit Shrivastava <shrivastavaprakrit@gmail.com>
2025-01-18 13:24:47 +0530

    modified page classes

--- src/test/java/testClass.java
+++ src/test/java/testClass.java
@@ -2,6 +2,8 @@ public class testClass {
 
     public static void searchPage(){
         System.out.println("Search Page");
+        System.out.println("The page has opened");
+        System.out.println("The page has closed");
     }
 
     public static void main(String[] args) {
