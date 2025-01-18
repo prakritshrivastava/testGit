b10720dfd8338286e2849a48ff2b12a2af7e1e70
Prakrit Shrivastava <shrivastavaprakrit@gmail.com>
2025-01-18 13:18:48 +0530

    add/updated files

---
+++ src/test/java/testClass.java
@@ -0,0 +1,13 @@
+public class testClass {
+
+    public static void searchPage(){
+        System.out.println("Search Page");
+    }
+
+    public static void main(String[] args) {
+
+        searchPage();
+
+    }
+
+}
